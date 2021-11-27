041c8e8f15a3bdfa12fca106d729c3e5a45b0145
Evgenii Korchagov <korchagov@mail.ru>
2021-11-27 19:51:01 +0300

    Done

--- main.cpp
+++ main.cpp
@@ -1,46 +1,53 @@
 #include <iostream>
 #include <ctime>
-#include<iomanip>
+#include <iomanip>
 #include <map>
+
+struct person
+{
+    std::string name;
+    std::tm birthdayDate;
+};
 
 int main()
 {
-    std::map<std::time_t, std::string> friends;
-    std::tm birthdayDate, todayDate;
+    person man;
+    std::map<std::time_t, person> friends;
+    std::tm nextBirthday, todayDate;
     std::time_t t;
 
     t=std::time(nullptr);
     todayDate=*std::localtime(&t);
-    birthdayDate=todayDate;
-    std::string name;
+    man.birthdayDate=todayDate;
 
     do
     {
         std::cout << "Input name and date of birth: ";
-        std::cin >> name;
-        if(name=="end")
+        std::cin >> man.name;
+        if(man.name=="end")
             break;
-        std::cin >> std::get_time(&birthdayDate, "%d/%m/%Y");
-        birthdayDate.tm_year=todayDate.tm_year;
+        std::cin >> std::get_time(&man.birthdayDate, "%d/%m/%Y");
+        nextBirthday=man.birthdayDate;
+        nextBirthday.tm_year=todayDate.tm_year;
 
-        t=std::mktime(&birthdayDate);
+        t=std::mktime(&nextBirthday);
         if(std::difftime(t, std::time(nullptr))<0)
         {
-            birthdayDate.tm_year++;
-            t=std::mktime(&birthdayDate);
+            nextBirthday.tm_year++;
+            t=std::mktime(&nextBirthday);
         }
 
         if(std::cin.fail())
             std::cin.clear();
         else
-            friends[t]=name;
+            friends[t]=man;
     }
     while(true);
 
-    //for(auto it=friends.begin();it!=friends.end();it++)
-    //{
-
-    //}
+    for(auto it=friends.begin();it!=friends.end();it++)
+    {
+        std::cout << it->second.name << " " << std::put_time(&(it->second.birthdayDate), "%d/%m/%Y") << std::endl;
+    }
 
     return 0;
 }
